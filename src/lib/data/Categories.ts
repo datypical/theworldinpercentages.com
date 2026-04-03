39b18f5515a8eb57d5e3880a6adb24d66ac5f8bf
Juanchi Parra <juanchiparra@gmail.com>
2026-04-03 14:54:44 -0400

    refactor(data): update categories and translations

---
+++ src/lib/data/Categories.ts
@@ -0,0 +1,9 @@
+export const CATEGORIES = [
+    { id: "economy", key: "catEconomy" },
+    { id: "education", key: "catEducation" },
+    { id: "environment", key: "catEnvironment" },
+    { id: "health", key: "catHealth" },
+    { id: "human rights", key: "catHumanRights" },
+    { id: "politics", key: "catPolitics" },
+    { id: "society", key: "catSociety" },
+] as const;
